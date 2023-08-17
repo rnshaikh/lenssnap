0d38843b77d2d2ab9c1edf3fad6819eeca188c44
Rizwan Shaikh <rshaikh@ces-ltd.com>
2023-08-17 23:34:53 +0530

    Added react spinner, login, navebar html

--- lenssnap_frontend/src/App.js
+++ lenssnap_frontend/src/App.js
@@ -1,13 +1,18 @@
-
-import './App.css';
+// import './App.css';
+import Login from './components/Login';
+import Spinner from './components/Spinner';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          Learn React
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //       Learn React
+    //   </header>
+    // </div>
+    //<Login></Login>
+    //<Spinner message="Hello World"></Spinner>
+    <Navbar></Navbar>
   );
 }
 
