0a6902a4cb61d2da1da7e6b4165c8db0d6e7f389
Rizwan Shaikh <rshaikh@ces-ltd.com>
2023-09-12 00:39:52 +0530

    added users find apis

--- lenssnap_backend/user_management/views/user_views.py
+++ lenssnap_backend/user_management/views/user_views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 
+from rest_framework.decorators import action
+
 from user_management.models import User
 from user_management.serializers import UserSerializerReadOnly
 
@@ -15,9 +17,13 @@ class UserProfileView(viewsets.ModelViewSet):
     queryset = User.objects.all()
 
     def list(self, request):
-
-        user = request.user
-        serializer = UserSerializerReadOnly(user)
+        import pdb
+        pdb.set_trace()
+        following_list = request.user.followers_by.all().values_list('followed_to__id', flat=True)
+        users = User.objects.exclude(
+                id__in=following_list
+                ).all()
+        serializer = UserSerializerReadOnly(users, many=True)
 
         return Response({
             "msg": "user detail fetch successfully",
@@ -36,6 +42,15 @@ class UserProfileView(viewsets.ModelViewSet):
     def destroy(self, request, pk):
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
+    @action(detail=False, methods=['GET'])
+    def profile(self, request):
+        user = request.user
+        serializer = UserSerializerReadOnly(user)
+
+        return Response({
+            "msg": "user detail fetch successfully",
+            "data": serializer.data
+            })
 
 
 
